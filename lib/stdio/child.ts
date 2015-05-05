282e4230db57b6f703f76d813159aa8a5e5a6bad
David Driscoll <david.driscoll@gmail.com>
2015-05-05 17:40:53 -0400

    Updated with more debugging it seems the project isn't get sent to omnisharp for some reason...

--- lib/stdio/child.ts
+++ lib/stdio/child.ts
@@ -1,8 +1,11 @@
 import {spawn, exec, ChildProcess} from "child_process";
 var argv = require('yargs').argv;
 
+console.log(argv);
 var serverPath = argv.serverPath;
+console.log('serverPath', serverPath);
 var projectPath = argv.projectPath;
+console.log('projectPath', projectPath);
 
 var childProcess = spawn(serverPath, ["--stdio", "-s", projectPath, "--hostPID", process.pid]);
 
